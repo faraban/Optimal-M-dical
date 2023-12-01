0adf96d98a7094e5136ea9c8053e7536b078efb7
ym-0-bm <93938113+ym-0-bm@users.noreply.github.com>
2023-12-01 17:18:55 +0000

    Auto stash before merge of "main" and "origin/main"

--- app.py
+++ app.py
@@ -8,40 +8,46 @@ app = Flask(__name__)
 def Connexion():
     return render_template("./connexion/connexion.html")
 
+
 @app.route('/monhopital')
 def monhopital():
     return render_template("./utilisateur/utilisateurhôpital.html")
+
 
 @app.route('/monprofil')
 def monprofil():
     return render_template("./utilisateur/utilisateurprofil.html")
 
+
 @app.route('/transfert')
 def transfert():
     return render_template("./utilisateur/utilisateurtransfert.html")
 
-#................brayane route (Inscription)#
+
+# ................brayane route (Inscription)#
 
 @app.route('/inscriptioninfos')
 def inscriptioninfos():
     return render_template("./inscription/inscriptioninfos.html")
 
+
 @app.route('/inscriptionacces')
 def inscriptionacces():
-    data=''
-    return render_template("./inscription/inscriptionacces.html",data=data)
+    data = ''
+    return render_template("./inscription/inscriptionacces.html", data=data)
+
 
 @app.route('/ListeService')
 def ListeService():
     return render_template("./inscription/inscriptionservice.html ")
+
 
 @app.route('/AjoutService')
 def AjoutService():
     return render_template("./inscription/inscriptionservice0.html ")
 
 
-
-#................Fin brayane route (Inscription)#
+# ................Fin brayane route (Inscription)#
 
 
 if __name__ == "__main__":
