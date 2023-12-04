32cb8de6e578fec0702decdcf301ce409f3bb07c
ym-0-bm <93938113+ym-0-bm@users.noreply.github.com>
2023-12-04 08:27:56 +0000

    code review

--- app.py
+++ app.py
@@ -4,9 +4,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 app = Flask(__name__)
 
 
-
-
-#  utilisateurs 
+#  utilisateurs
 
 
 @app.route('/monhopital')
@@ -38,22 +36,20 @@ def inscriptionacces():
 
 
 @app.route('/ListeService')
-def ListeService():
+def listeservice():
     return render_template("./inscription/inscriptionservice.html ")
 
 
 @app.route('/AjoutService')
-def AjoutService():
+def ajoutservice():
     return render_template("./inscription/inscriptionservice0.html ")
 
 
-<<<<<<< HEAD
 # ................Fin brayane route (Inscription)#
-=======
 #     connexion 
 
 @app.route('/')
-def Connexion():
+def connexion():
     return render_template("./connexion/connexion.html")
 
 
@@ -61,16 +57,18 @@ def Connexion():
 def pwdcode():
     return render_template("./connexion/pwdcode.html")
 
+
 @app.route('/pwdreset')
 def pwdreset():
     return render_template("./connexion/pwdreset.html")
+
 
 @app.route('/pwdoublie')
 def pwdoublie():
     return render_template("./connexion/pwdoublie.html")
 
-#................Fin brayane route (Inscription)#
->>>>>>> 6ffb3c69d4e9d0da3abb3d9800ab5675e0982269
+
+# ................Fin brayane route (Inscription)#
 
 
 if __name__ == "__main__":
